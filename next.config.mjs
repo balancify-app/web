22d4d93efb35b48f6e5a7a07b745f3132091ee44
Layhout <152836685+Layhout-WB@users.noreply.github.com>
2024-04-19 17:19:20 +0700

    chore: create profile config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    swcPlugins: [['@swc-jotai/react-refresh', {}]],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  transpilePackages: ['lucide-react'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*', // Match all domains and subdomains
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
