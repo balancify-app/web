e0615e9ee698486aa11a7f25d8385c0fdbd8f76c
Sokheng Hing <148696295+hing-sokheng@users.noreply.github.com>
2024-04-20 14:05:05 +0700

    Update page.tsx

--- app/app/dashboard/page.tsx
+++ app/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ export default function Dashboard() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <div className="flex items-center">
-        <h1 className="border-r-[1px] border-gray-400 p-2  px-4">Under Construction</h1>
+        <h1 className="border-r-[1px] border-gray-400 p-2 px-4">Under Construction</h1>
         <h1 className="p-2 px-4">Dashboard page</h1>
       </div>
     </div>
