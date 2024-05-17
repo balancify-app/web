ae0180c0fda3af180c7674807294012db0d366e4
Layhout <152836685+Layhout-WB@users.noreply.github.com>
2024-05-17 17:16:16 +0700

    chore: create blog page

--- app/blogs/_components/BlogListItem.tsx
+++ app/blogs/_components/BlogListItem.tsx
@@ -7,9 +7,9 @@ type BlogListItemProps = Readonly<{
 
 export default function BlogListItem({ children, date, title, version }: BlogListItemProps) {
   return (
-    <li className="group flex gap-8 md:gap-14">
-      <div className="pt-0.5">
-        <h1 className="hidden text-right text-sm text-gray-400 md:block">{date}</h1>
+    <li className="group flex gap-8 pl-2 md:gap-14 md:pl-0">
+      <div className="hidden pt-0.5 md:block">
+        <h1 className=" text-right text-sm text-gray-400 ">{date}</h1>
       </div>
       <div className="relative w-0.5 translate-y-3 bg-border group-last:bg-transparent">
         <div className="relative left-0 top-0 h-3.5 w-3.5 -translate-x-1.5 -translate-y-1/2 rounded-full bg-border after:absolute after:left-1/2 after:top-1/2 after:h-2/3 after:w-2/3 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full after:bg-background" />
