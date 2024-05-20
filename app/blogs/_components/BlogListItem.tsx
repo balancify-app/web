36352e5f80bb4a77c3ee7353a8b5243f485df9f8
Layhout <152836685+Layhout-WB@users.noreply.github.com>
2024-05-20 08:55:32 +0700

    chore: create blog page

--- app/blogs/_components/BlogListItem.tsx
+++ app/blogs/_components/BlogListItem.tsx
@@ -12,7 +12,9 @@ export default function BlogListItem({ children, date, title, version }: BlogLis
         <h1 className=" text-right text-sm text-gray-400 ">{date}</h1>
       </div>
       <div className="relative w-0.5 translate-y-3 bg-border group-last:bg-transparent">
-        <div className="relative left-0 top-0 h-3.5 w-3.5 -translate-x-1.5 -translate-y-1/2 rounded-full bg-border" />
+        <div className="relative left-0 top-0 flex h-3.5 w-3.5 -translate-x-1.5 -translate-y-1/2 items-center justify-center rounded-full bg-border">
+          <div className="h-2 w-2 rounded-full bg-background"></div>
+        </div>
       </div>
       <div className="flex-1 pb-12">
         <h1 className="font-bold">{title}</h1>
